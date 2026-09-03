bf1d4e4e53c1edea65c42a9f2ad5f92f8a2df8fc
BelVolnyStalkerDev <arsenyshumchyk@gmail.com>
2026-09-03 21:37:38 +0200

    Updated PDA-System.lua content

--- PDA-System.lua
+++ PDA-System.lua
@@ -1,3 +1,9 @@
 local PDA-SystemVersion = "PDA Vers. Pre-Alpha 0.0001"
 
 print("\27[37mКПК vers. 0.0001\27[37m")
+print("Создано Сталкерами для Сталкеров")
+print("[1]Включить Питание")
+print("[2]Выключить Питание")
+print("")
+io.write("Выбирай Сталкер")
+local pr1nt = io.read()
