60234479831c4927f2cd5f2989cd64af1a270124
BelVolnyStalkerDev <arsenyshumchyk@gmail.com>
2026-09-04 20:58:05 +0200

    Update PDA-System.lua

--- PDA-System.lua
+++ PDA-System.lua
@@ -1,9 +1,18 @@
 local PDA-SystemVersion = "PDA Vers. Pre-Alpha 0.0001"
 
 print("\27[37mКПК vers. 0.0001\27[37m")
-print("Создано Сталкерами для Сталкеров")
+print("Создано Сталкерами для Сталкеров©")
 print("[1]Включить Питание")
 print("[2]Выключить Питание")
 print("")
 io.write("Выбирай Сталкер")
 local pr1nt = io.read()
+
+if pr1nt == "1" or pr1nt == "Включить Питание" then
+  print (\27[37mКПК vers. 0.0001\27[37m)
+  os.execute("sleep 2")
+  print("")
+else
+  print("Ошибка!Не Правильная команда Попробуйте заново!")
+  os.execute("sleep")
+  os.execute("clear")
